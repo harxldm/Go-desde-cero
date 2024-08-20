07a2c2941d421fab9ea99deacb1389ceca8ee11a
Harold <harxldm@gmail.com>
2024-08-19 23:33:41 -0500

    Go desde cero

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*cat, dog := "milo", "benny"
+
+	fmt.Println(cat, dog)
+	*/
+	a, b := 100, 23000
+
+	res := a + b
+
+	fmt.Printf("Tipo: %T, Valor: %v", res, res)
+
+}
+
+// esto es un comentario
+
+/* esto tambien es un comentario*/
